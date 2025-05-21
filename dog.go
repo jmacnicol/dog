b4b032abc58345bffc8a2e0ed2df82db180f8c82
jmacnicol <jmacnicol@opentext.com>
2025-05-21 14:21:45 -0600

    Adding Years utility function

--- dog.go
+++ dog.go
@@ -4,7 +4,15 @@ import (
 	"strings"
 )
 
+// DOG_YEARS is a constant representing 7 years.
+const DOG_YEARS = 7
+
 // pushing this
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up, it says " + strings.ToUpper(s)
+}
+
+// Years takes human years as an input and converts to dog years.
+func Years(human int) int {
+	return human * DOG_YEARS
 }
